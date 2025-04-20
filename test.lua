9ab115b454d7368118dc926eedd0236516f80049
tw-zuba <dinhthang.32511@gmail.com>
2025-04-20 21:04:58 +0700

    Add files via upload

---
+++ test.lua
@@ -0,0 +1,84 @@
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/congaincc/Ui-Redz-V2/refs/heads/main/KoBtne.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Ghét Cgai",
+         Animation = "Youtube: Ko Bt"
+         },
+        Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
+
+       MinimizeButton({
+       Image = "https://create.roblox.com/store/asset/80744147783753/Screenshot-20250420-205109",
+       Size = {60, 60},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Script Farm"})
+     
+------- BUTTON
+    
+    AddButton(Tab1o, {
+     Name = "Redz Hub",
+    Callback = function()
+	  local Settings = {
+  JoinTeam = "Pirates"; -- Pirates/Marines
+  Translator = true; -- true/false
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
+  end
+  })
+
+    AddButton(Tab1o, {
+     Name = "Zis Hub",
+    Callback = function()
+	  local Settings = {
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Zis/refs/heads/main/ZisRb8"))()
+
+    AddButton(Tab1o, {
+     Name = "Aim Lock",
+    Callback = function()
+	  local Settings = {
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VanThanhIOS/AlexAimBot/refs/heads/main/AimBotBanTumLum.lua.txt"))()
+
+    AddButton(Tab1o, {
+     Name = "Speed X Hub",
+    Callback = function()
+	  local Settings = {
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))() 
+
+    AddButton(Tab1o, {
+     Name = "Xero Hub",
+    Callback = function()
+	  local Settings = {
+getgenv().Team = "Marines"
+getgenv().Hide_Menu = false
+getgenv().Auto_Execute = false
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xero2409/XeroHub/refs/heads/main/main.lua"))() 
+
+    AddButton(Tab1o, {
+     Name = "Quan Tâm Hub",
+    Callback = function()
+	  local Settings = {
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Trustmenotcondom/QTONYX/refs/heads/main/QuantumOnyx.lua"))()
+
+
+
+
